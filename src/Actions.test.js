ca160d05dd03354f6bfbd98206baaa0b02c685af
Jonathan Chhabra <Sonicrida@users.noreply.github.com>
2017-08-13 14:54:36 -0400

    Create basic test for requestUserData action creator

---
+++ src/Actions.test.js
@@ -0,0 +1,11 @@
+import * as actions from './actions';
+import * as types from './constants/actionTypes';
+
+describe('actions', () => {
+    it('Should create an action to begin fetching data', () => {
+      const expectedAction = {
+        type: types.FETCH_USERS_REQUEST
+      }
+      expect(actions.requestUserData()).toEqual(expectedAction)
+    })
+  })
